d901ff46493877a8dfaccc160059c42dd653a291
Abdelrahman <eng.abdelrahman.mostafa.soliman@gmail.com>
2022-05-06 11:36:51 +0200

    Refactoring RLH

--- Cdr_Processor/src/main/java/Rating.java
+++ Cdr_Processor/src/main/java/Rating.java
@@ -104,73 +104,41 @@ public class Rating {
     }
 
     public static void RLH(CDR cdr, Integer nDuration) throws SQLException {
-        //check if user has any free units and re rate the service
-        //look in the additional_sp column
-        //submit to db consumption table
         String Service_type = SiteDAO.instanceData.getService(cdr.getService_id());
-        int oldDuration = cdr.getDuration();
-        int nFree = 0;
-        String str=null;
         int FreeU = SiteDAO.instanceData.getAddFreeUnits(cdr.getSource_msisdn());
-        if (FreeU == -1) {
+        RatePlane currentRatePlan = SiteDAO.instanceData.getRatePlane(cdr.getRatePlan_id()).get(0);
+        String str=serviceTypeMapping(Service_type);
 
-            // add data to db
-        } else {
+        int oldDuration = cdr.getDuration();
+        int nFree = 0, price = 0;
+
+        if (FreeU != -1) {
             if (cdr.getRate() == 0) {
                 //duration
-                if (nDuration == 0) {
-                    //use cdr.duration
-                    if (oldDuration > FreeU) {
-                        nDuration = oldDuration - FreeU;
-                    } else if (oldDuration <= FreeU) {
-                        nFree = FreeU - oldDuration;
-                    }
-                } else {
-                    // use nduration
-                    if (nDuration > FreeU) {
-                        nDuration = nDuration - FreeU;
-                    } else if (oldDuration <= FreeU) {
-                        nFree = FreeU - nDuration;
-                    }
-                }
+                if (nDuration == 0)  nDuration=oldDuration;
+                // use nduration
+                if (nDuration > FreeU) nDuration = nDuration - FreeU;
+                else nFree = FreeU - nDuration;
+
             } else {
-                // rate
-                // rating again
-                // duratin ? service type ? additional service ?
-                // duration > free units  duration - freeunitsd =?
-                // new rate = ? * additiopnal
-                // duration < fre u      free units - duration =?
-                // current add sp =?
                 if (oldDuration > FreeU) {
                     nDuration = oldDuration - FreeU;
-
-                    RatePlane currentRatePlan = SiteDAO.instanceData.getRatePlane(cdr.getRatePlan_id()).get(0);
-                    int price = 0;
-                    switch (Service_type) {
-                        case "voice":
-                        case "cross_voice":
-                            price = currentRatePlan.getAdditional_minutes_service();
-                            break;
-                        case "sms":
-                            price = currentRatePlan.getAdditional_sms_service();
-                            break;
-                        case "data":
-                            price = currentRatePlan.getAdditional_data_service();
-                            break;
-                        case "roaming":
-                            price = currentRatePlan.getAdditional_roaming_service();
-                            break;
-                    }
+                    price=getPrice(currentRatePlan,Service_type);
                     cdr.setRate(price * nDuration);
                     nDuration = 0;
-                } else if (oldDuration <= FreeU) {
+                } else {
                     nFree = FreeU - oldDuration;
                     cdr.setRate(0);
                 }
             }
         }
-        //add to db
-        // add to contract database
+        int units= SiteDAO.instanceData.getUnits(cdr.getSource_msisdn(),str);
+        units =units-nDuration;
+        SiteDAO.instanceData.setUnits(cdr.getSource_msisdn(),str,units,nFree);
+        SiteDAO.instanceData.setRTX(cdr);
+    }
+    private static String serviceTypeMapping(String Service_type){
+        String str =null;
         switch (Service_type){
             case "voice":
                 str="current_voice";
@@ -188,9 +156,26 @@ public class Rating {
                 str= "current_roaming";
                 break;
         }
-        int units= SiteDAO.instanceData.getUnits(cdr.getSource_msisdn(),str);
-        units =units-nDuration;
-        SiteDAO.instanceData.setUnits(cdr.getSource_msisdn(),str,nDuration,nFree);
-        SiteDAO.instanceData.setRTX(cdr);
+        return str;
+    }
+    private static int getPrice(RatePlane ratePlane ,String Service_type){
+
+        int price = 0;
+        switch (Service_type) {
+            case "voice":
+            case "cross_voice":
+                price = ratePlane.getAdditional_minutes_service();
+                break;
+            case "sms":
+                price = ratePlane.getAdditional_sms_service();
+                break;
+            case "data":
+                price = ratePlane.getAdditional_data_service();
+                break;
+            case "roaming":
+                price = ratePlane.getAdditional_roaming_service();
+                break;
+        }
+        return price;
     }
 }
