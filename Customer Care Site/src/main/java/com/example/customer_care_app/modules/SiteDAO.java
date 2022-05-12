631278e4a0f2fddbdebde14540ad88ea10a9fd96
Abdelrahman <eng.abdelrahman.mostafa.soliman@gmail.com>
2022-05-12 16:19:21 +0200

    view Invoice

--- Customer Care Site/src/main/java/com/example/customer_care_app/modules/SiteDAO.java
+++ Customer Care Site/src/main/java/com/example/customer_care_app/modules/SiteDAO.java
@@ -1,5 +1,8 @@
 package com.example.customer_care_app.modules;
 
+import net.sf.jasperreports.engine.JRException;
+
+import java.io.FileNotFoundException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -385,4 +388,113 @@ public class SiteDAO {
         }
         return users;
     }
+    //needed functions to have the invoice website
+    public static List<Bill_Info> generateBillsForUser(int NID) throws SQLException, JRException, FileNotFoundException {
+
+        List<Bill_Info> numbersBills = new ArrayList<>();
+        List<String> userNumbers = SiteDAO.instanceData.getUserMSISDNs(NID);
+
+        for (String msisdn: userNumbers) {
+            numbersBills.add(SiteDAO.instanceData.billCycle(msisdn));
+        }
+        return numbersBills;
+    }
+    public List<String> getUserMSISDNs(int userID) throws SQLException {
+        stmt = this.con.prepareStatement("select msisdn from bscs.contract where userid = ?");
+        stmt.setInt(1,userID);
+        ResultSet rs = stmt.executeQuery();
+        List<String> MSISDNs = new ArrayList<>();
+
+        while(rs.next()){
+            MSISDNs.add(rs.getString("msisdn"));
+        }
+        return MSISDNs;
+    }
+    public static Bill_Info billCycle(String msisdn) throws SQLException {
+        Bill_Info contractBill = new Bill_Info();
+        //Apply Bill Cycle Queries to retrieve required Data
+        ContractCons cont = SiteDAO.instanceData.getUserRatePlaneInfo(msisdn);
+
+        int acv = SiteDAO.instanceData.getAddConsumedUnits(msisdn,1); // Additional Consumed Unit for OnNet voice
+        int racv = SiteDAO.instanceData.getRatedAddUnits(msisdn,1); // Total Rating for Additional Consumed Unit for OnNet voice
+
+        int acc = SiteDAO.instanceData.getAddConsumedUnits(msisdn,2); // Additional Consumed Unit for CrossNet voice
+        int racc = SiteDAO.instanceData.getRatedAddUnits(msisdn,2); // Total Rating for Additional Consumed Unit for CrossNet voice
+
+        int acd = SiteDAO.instanceData.getAddConsumedUnits(msisdn,3); // Additional Consumed Unit for Data
+        int racd = SiteDAO.instanceData.getRatedAddUnits(msisdn,3); // Total Rating for Additional Consumed Unit for Data
+
+        int acs = SiteDAO.instanceData.getAddConsumedUnits(msisdn,4); // Additional Consumed Unit for SMS
+        int racs = SiteDAO.instanceData.getRatedAddUnits(msisdn,4); // Total Rating for Additional Consumed Unit for SMS
+
+        int acr = SiteDAO.instanceData.getAddConsumedUnits(msisdn,5); // Additional Consumed Unit for Roaming
+        int racr = SiteDAO.instanceData.getRatedAddUnits(msisdn,5); // Total Rating for Additional Consumed Unit for Roaming
+
+        // Calculations
+        int totalExtraFees = (racv + racc + racd + racs + racr)/100;
+        int totalBillFees = cont.getFee() + totalExtraFees;
+
+        // Mapping values to the class
+        contractBill.setMsisdn(msisdn);
+        contractBill.setMonthlyFees(cont.getFee());
+        contractBill.setRateplane(cont.getRateplaneName());
+
+        contractBill.setExtraConsumedVoice(acv);
+        contractBill.setRatedExtraVoice(racv/100);
+
+        contractBill.setExtraConsumedCross(acc);
+        contractBill.setRatedExtraCross(racc/100);
+
+        contractBill.setExtraConsumedData(acd);
+        contractBill.setRatedExtraData(racd/100);
+
+        contractBill.setExtraConsumedSMS(acs);
+        contractBill.setRatedExtraSMS(racs/100);
+
+        contractBill.setExtraConsumedRoaming(acr);
+        contractBill.setRatedExtraRoaming(racr/100);
+
+        contractBill.setExtraFees(totalExtraFees);
+        contractBill.setTotalFees(totalBillFees);
+
+        return contractBill;
+    }
+    public ContractCons getUserRatePlaneInfo(String msisdn)throws SQLException{
+        stmt= this.con.prepareStatement("select r.commercial_name,r.fee from bscs.contract as c  join bscs.rateplanes as r on c.rateplane_id = r.id where c.msisdn = ?");
+        stmt.setString(1,msisdn);
+        ResultSet rs = stmt.executeQuery();
+        ContractCons cont = new ContractCons();
+        while(rs.next()){
+            cont.setRateplaneName(rs.getString("commercial_name"));
+            cont.setFee(rs.getInt("fee"));
+        }
+        return cont;
+    }
+    public int getAddConsumedUnits(String msisdn, int serviceID) throws SQLException{
+        stmt = this.con.prepareStatement("select sum(duration) from rtx.consumption where source_msisdn = ? and service_id= ? and rate != '0';");
+        stmt.setString(1,msisdn);
+        stmt.setInt(2,serviceID);
+        ResultSet rs = stmt.executeQuery();
+        int totalExtraConsumed = 0;
+
+        while(rs.next())
+        {
+            totalExtraConsumed = rs.getInt("sum");
+        }
+        return totalExtraConsumed;
+    }
+    public int getRatedAddUnits(String msisdn, int serviceID) throws SQLException{
+        stmt = this.con.prepareStatement("select sum(rate) from rtx.consumption where source_msisdn = ? and service_id= ?;");
+        stmt.setString(1,msisdn);
+        stmt.setInt(2,serviceID);
+        ResultSet rs = stmt.executeQuery();
+        int totalRate = 0;
+
+        while(rs.next())
+        {
+            totalRate = rs.getInt("sum");
+        }
+        return totalRate;
+    }
+
 }
